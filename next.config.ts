b4c37340d7d1dd0c28fb60c38b9ce38fcab82ec2
HrishavSaha <hrishav.saha@gmail.com>
2025-04-14 22:26:13 +0530

    removed secrets

--- next.config.ts
+++ next.config.ts
@@ -4,11 +4,4 @@ const nextConfig: NextConfig = {
   /* config options here */
 };
 
-module.exports = {
-    env: {
-        DATABASE_URL: "postgresql://postgres:gcgxPBxNQGyiDgnLTOXQiuwNoxTlKtgl@mainline.proxy.rlwy.net:28900/railway",
-        RESEND_API_KEY: "re_7KNo1UKB_BRGzNibk1vA154L815qHgnhf"
-    },
-}
-
 export default nextConfig;
