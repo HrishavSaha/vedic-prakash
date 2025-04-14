94a536d0228208b4d4b3c4f3913e0ea832b272d2
HrishavSaha <hrishav.saha@gmail.com>
2025-04-14 21:30:20 +0530

    trying to make the prod server work

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,7 @@ const nextConfig: NextConfig = {
 
 module.exports = {
     env: {
+        DATABASE_URL: "postgresql://postgres:gcgxPBxNQGyiDgnLTOXQiuwNoxTlKtgl@mainline.proxy.rlwy.net:28900/railway"
         RESEND_API_KEY: "re_7KNo1UKB_BRGzNibk1vA154L815qHgnhf"
     },
 }
