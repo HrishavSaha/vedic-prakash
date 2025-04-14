c66462610634df10b7bf506f501d0ef28c7c38d2
HrishavSaha <hrishav.saha@gmail.com>
2025-04-14 23:41:02 +0530

    deleted cards

--- app/api/cards/route.ts
+++
@@ -1,7 +0,0 @@
-import { PrismaClient } from "@prisma/client"
-const prisma = new PrismaClient()
-
-export async function GET() {
-    const cards = await prisma.card.findMany()
-    return Response.json(cards)
-}
