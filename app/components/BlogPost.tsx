ae353944269523bbba6c0e0235f930c6adb4b942
HrishavSaha <hrishav.saha@gmail.com>
2025-04-19 17:01:00 +0530

    style(BlogPost): adjust transition duration and button styling
    Update the transition duration from 300ms to 500ms for smoother animations. Modify the button styling to remove shadows, adjust width, and align the title text properly. These changes improve the visual consistency and user experience of the BlogPost component.

--- app/components/BlogPost.tsx
+++ app/components/BlogPost.tsx
@@ -47,25 +47,25 @@ export default function BlogPost () {
             {blogs.map((blog, index) => (
                 <div
                 key={index}
-                className={`container flex flex-col w-full h-fit mx-auto p-4 transition-all duration-300 bg-white rounded-xl ${
+                className={`container flex flex-col w-full h-fit mx-auto p-4 transition-all duration-500 bg-white rounded-xl ${
                     isExpanded && currentBlog == index && isClient
                     ? "gap-4"
                     : "gap-0"
                 }`}>
                     <div className="flex">
                         <Button
-                        className="bg-transparent hover:bg-gray-200 text-black text-6xl p-0"
+                        className="bg-transparent w-full flex justify-start shadow-none hover:bg-transparent text-black text-6xl p-0"
                         onClick={() => handleClick(index)}
                         >
                             {isExpanded && currentBlog == index && isClient ? 
-                            <PiCaretUpBold className="w-6 h-6"/> :
+                            <PiCaretUpBold className="w-10 h-10"/> :
                             <PiCaretDownBold className="w-10 h-10"/>}
+                            <h2 className="text-2xl font-bold">
+                                {blog.title}
+                            </h2>
                         </Button>
-                        <h2 className="text-3xl font-bold">
-                            {blog.title}
-                        </h2>
                     </div>
-                    <div className={`pl-10 transition-all duration-300 ${
+                    <div className={`pl-10 transition-all duration-500 ${
                         isExpanded && currentBlog == index && isClient
                         ? "max-h-[200px] overflow-auto"
                         : "max-h-0 overflow-hidden"
